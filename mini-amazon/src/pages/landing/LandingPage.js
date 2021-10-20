ad670562257dc60a5fe1a709f5aee95f6ee3a236
lorneez <lornz17@gmail.com>
2021-10-19 23:45:00 -0400

    Added check for logged in user at Landing Page

--- mini-amazon/src/pages/landing/LandingPage.js
+++ mini-amazon/src/pages/landing/LandingPage.js
@@ -1,7 +1,28 @@
-import React from "react"
+import React, {useEffect, useContext} from "react"
 import SideBarComponent from "../../components/SideBarComponent";
+import { useHistory } from "react-router-dom";
+import AuthContext from "../../contexts/AuthContext";
 
 function LandingPage() {
+    const auth = useContext(AuthContext);
+    const { state } = auth;
+    const { isSignedIn, userType } = state;
+
+    useEffect(()=> {
+        const { dispatch } = auth;
+        if(isSignedIn) {
+            if(userType === "buyer") {
+                history.push("/dashboard");
+            }
+            else if(userType === "seller") {
+                history.push("/seller/dashboard");
+            }
+            else {
+                // TODO: Handle incorrect user type.
+            }
+        }
+    }, []);
+
     return (
         <div>
             <div className={"columns"}>
