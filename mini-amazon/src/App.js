49b79ab788bc1ee5d922eda3e6dc628cfc25e49b
lorneez <lornz17@gmail.com>
2021-10-19 23:42:01 -0400

    Added AuthStore to App

--- mini-amazon/src/App.js
+++ mini-amazon/src/App.js
@@ -16,28 +16,32 @@ import Reviews from "./pages/seller/Reviews";
 import SellerOrders from "./pages/seller/SellerOrders";
 import Inventory from "./pages/seller/Inventory";
 import ProtectedRoute from "./components/protected/ProtectedRoute";
+import { AuthStore } from './contexts/AuthContext';
+
 
 function App() {
     return (
-        <BrowserRouter>
-            <Switch>
-                <Route path="/" exact component={LandingPage} />
-                <Route path="/login" exact component={LoginPage} />
-                <Route path="/sign-up" exact component={SignUpPage} />
-                <ProtectedRoute path="/product/browse" exact component={ProductBrowse} />
-                <ProtectedRoute path="/product/details" exact component={ProductDetails} />
-                <ProtectedRoute path="/product/review" exact component={ProductReview} />
-                <ProtectedRoute path="/product/create" exact component={CreateProduct} />
-                <ProtectedRoute path="/dashboard" exact component={BuyerDashboard} />
-                <ProtectedRoute path="/orders" exact component={Orders} />
-                <ProtectedRoute path="/search" exact component={Search} />
-                <ProtectedRoute path="/cart" exact component={Cart} />
-                <ProtectedRoute path="/seller/dashboard" exact component={SellerDashboard} />
-                <ProtectedRoute path="/seller/inventory" exact component={Inventory} />
-                <ProtectedRoute path="/seller/orders" exact component={SellerOrders} />
-                <ProtectedRoute path="/seller/reviews" exact component={Reviews} />
-            </Switch>
-        </BrowserRouter>
+        <AuthStore>
+            <BrowserRouter>
+                <Switch>
+                    <Route path="/" exact component={LandingPage} />
+                    <Route path="/login" exact component={LoginPage} />
+                    <Route path="/sign-up" exact component={SignUpPage} />
+                    <ProtectedRoute path="/product/browse" exact component={ProductBrowse} />
+                    <ProtectedRoute path="/product/details" exact component={ProductDetails} />
+                    <ProtectedRoute path="/product/review" exact component={ProductReview} />
+                    <ProtectedRoute path="/product/create" exact component={CreateProduct} />
+                    <ProtectedRoute path="/dashboard" exact component={BuyerDashboard} />
+                    <ProtectedRoute path="/orders" exact component={Orders} />
+                    <ProtectedRoute path="/search" exact component={Search} />
+                    <ProtectedRoute path="/cart" exact component={Cart} />
+                    <ProtectedRoute path="/seller/dashboard" exact component={SellerDashboard} />
+                    <ProtectedRoute path="/seller/inventory" exact component={Inventory} />
+                    <ProtectedRoute path="/seller/orders" exact component={SellerOrders} />
+                    <ProtectedRoute path="/seller/reviews" exact component={Reviews} />
+                </Switch>
+            </BrowserRouter>
+        </AuthStore>
     );
 }
 
