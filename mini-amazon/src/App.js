d9a15aeda904289046dbc6e73d43944505f8c19c
Brian <librian2000@gmail.com>
2021-10-19 23:04:42 -0400

    added route for product details

--- mini-amazon/src/App.js
+++ mini-amazon/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import HomePage from "./pages/HomePage";
+import ProductBrowse from "./pages/product/ProductBrowse";
 
 function App() {
     return (
         <BrowserRouter>
             <Switch>
                 <Route path="/" exact component={HomePage} />
+                <Route path="/product/browse" exact component={ProductBrowse} />
             </Switch>
         </BrowserRouter>
     );
---
+++ mini-amazon/src/pages/product/ProductBrowse.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+function ProductBrowse() {
+    return (
+        <div className={"container"}>
+            Browse For Products
+        </div>
+    )
+}
+
+export default ProductBrowse;
