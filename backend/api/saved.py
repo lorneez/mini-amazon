fa54ae85db44d6c132daa16df7674cd3c9fea5f6
Jin Cho <jcho.1260@gmail.com>
2021-11-08 01:07:25 -0500

    added ability to add to the saved list for a user

--- backend/api/saved.py
+++ backend/api/saved.py
@@ -6,4 +6,15 @@ app = Flask(__name__)*b
 
 @app.route("/api/user_all_saved", methods=["GET"])
 def all_saved():
-    return Saved.get_all_user(incoming["user_id"])
+    return Saved.get_all_user(request.args["user_id"])
+
+@app.route("/api/add_saved", methods=["POST"])
+def add_saved_item():
+    '''
+    '''
+    if not Saved.product_exists(request.args['user_id'], request.args['product_id']):
+        saved_prod = Saved.add_product(request.args['user_id'], request.args['product_id'])
+    if saved_prod is None:
+        flash('Could not save item to account')
+        return None
+    return jsonify(product = request.args['product_id'])
--- backend/models/saved.py
+++ backend/models/saved.py
@@ -1,4 +1,5 @@
 from flask import current_app as app
+from models.product import Product
 
 class Saved:
     def __init__(self, id, user_id, product_id):
@@ -29,3 +30,28 @@ WHERE id IN (
 ORDER BY time_stamp DESCENDING
 ''')
         return [Cart(*row) for row in rows]
+
+    @staticmethod
+    def product_exists(uid, pid):
+        rows = app.db.execute('''
+    SELECT *
+    FROM SavedItem
+    WHERE user_id = uid 
+    AND product_id = pid
+    ''')
+        return len(rows)>0
+
+    @staticmethod
+    def add_product(uid, pid):
+        try:
+            rows = app.db.execute("""
+INSERT INTO SavedItem(id, user_id, product_id)
+VALUES(NEWID(), uid, pid)
+RETURNING pid
+""")
+            pid = rows[0][0]
+            return Product.get(pid)
+        except Exception:
+            return None
+
+    
