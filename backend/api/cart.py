c4d1b01b1146c9b5b770affea1b1b57ffc269bc3
Jin Cho <jcho.1260@gmail.com>
2021-11-07 23:09:19 -0500

    corrected method call input from incoming get call for cart

--- backend/api/cart.py
+++ backend/api/cart.py
@@ -6,4 +6,5 @@ app = Flask(__name__)
 
 @app.route("/apiuser_cart", methods=["GET"])
 def all_products():
-    return Cart.get_all_user
+    incoming = request.get_json()
+    return Cart.get_all_user(incoming["user_id"])
