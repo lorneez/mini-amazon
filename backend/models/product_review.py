5ebca8fee9b9b05a9e77269edfe92374592ebb59
Jin Cho <jcho.1260@gmail.com>
2021-11-08 02:44:21 -0500

    added default start vote and star for reviews

---
+++ backend/models/product_review.py
@@ -0,0 +1,81 @@
+from flask import current_app as app
+
+class ProductReview:
+    def __init__(self, from_id, product_id):
+        self.id = id
+        self.uid = user_id
+        self.pid = product_id
+
+    @staticmethod
+    def get(id):
+        rows = app.db.execute('''
+SELECT *
+FROM SavedItem
+WHERE id = :id
+''',
+                              id=id)
+        return SavedItem(*(rows[0])) if rows is not None else None
+
+    @staticmethod
+    def get_all_user(uid):
+        rows = app.db.execute('''
+SELECT *
+FROM Products
+WHERE id IN (
+    SELECT product_id
+    FROM SavedItem
+    WHERE user_id = :uid
+)
+ORDER BY time_stamp DESCENDING
+''', uid=uid)
+        return [Cart(*row) for row in rows]
+
+    @staticmethod
+    def product_exists(uid, pid):
+        rows = app.db.execute('''
+    SELECT *
+    FROM SavedItem
+    WHERE user_id = :uid 
+    AND product_id = :pid
+    ''', uid=uid, pid=pid)
+        return len(rows)>0
+
+    @staticmethod
+    def update_time(uid, pid):
+        try:
+            rows = app.db.execute('''
+            UPDATE SavedItem
+            SET product_id = :pid
+            WHERE user_id = :uid 
+            AND product_id = :pid
+            RETURNING pid
+            ''', uid=uid, pid=pid)
+            pid = rows[0][0]
+            return Product.get(pid)
+        except Exception:
+            return None
+
+    @staticmethod
+    def add_product(uid, pid):
+        try:
+            rows = app.db.execute("""
+INSERT INTO SavedItem(id, user_id, product_id)
+VALUES(NEWID(), :uid, :pid)
+RETURNING :pid
+""", uid=uid, pid=pid)
+            pid = rows[0][0]
+            return Product.get(pid)
+        except Exception:
+            return None
+
+    @staticmethod
+    def remove_product(uid, pid):
+        if cart_item_exists(uid, pid):
+            try:
+                rows = app.db.execute("""
+                DELETE FROM SavedItem
+                WHERE uid=:uid
+                AND pid=:pid
+                """, uid=uid, pid=pid)
+            except Exception:
+                return None
