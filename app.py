baaa49f7cbab205df4c305e698abe9d7f6c97603
lorneez <lornz17@gmail.com>
2021-11-10 21:58:39 -0500

    Another attempt

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from  backend.app import create_app
 app = create_app()
 
-from backend.app import products
+from backend.app.products import products
+app.register_blueprint(products)
 
 @app.route('/hello/', methods=['GET', 'POST'])
 def welcome():
--- backend/app/products.py
+++ backend/app/products.py
@@ -1,30 +1,28 @@
-from flask import Flask, request
+from flask import Flask, request, Blueprint
 from backend.app.models.product_review import PReview
 from backend.app.models.product import Product
 
 
 # Configure application
-from  backend.app import create_app
+products = Blueprint('products',__name__)
 
-app = create_app()
-
-@app.route("/api/all_products/", methods=["GET"])
+@products.route("/api/all_products/", methods=["GET"])
 def all_products():
     print("Successfully called!")
     products = Product.get_all()
     return json.dumps([item.__dict__ for item in products])
 
-@app.route("/api/products_category", methods=["GET"])
+@products.route("/api/products_category", methods=["GET"])
 def all_products_category():
     products = Product.get_category(request.args['category'])
     return json.dumps([item.__dict__ for item in products])
 
-@app.route("/api/products_seller", methods=["GET"])
+@products.route("/api/products_seller", methods=["GET"])
 def all_products_seller():
     products = Product.get_by_seller(request.args['seller_id'])
     return json.dumps([item.__dict__ for item in products])
 
-@app.route("/api/all_product_reviews", methods=["GET"])
+@products.route("/api/all_product_reviews", methods=["GET"])
 def all_product_reviews():
     reviews = PReview.get_all_for_product(request.args['product_id'])
     return json.dumps([r.__dict__ for r in reviews])
