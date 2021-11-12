270e619d5e9fc920db346f53efb8cd03d5cd78b3
Jin Cho <jc695@duke.edu>
2021-11-11 21:38:05 -0500

    added product review endpoint for getting reviews

--- app.py
+++ app.py
@@ -6,12 +6,14 @@ from backend.app.cart import cart
 from backend.app.orders import orders
 from backend.app.saved import saved
 from backend.app.users import users
+from backend.app.product_reviews import previews
 
 app.register_blueprint(products)
 app.register_blueprint(cart)
 app.register_blueprint(orders)
 app.register_blueprint(saved)
 app.register_blueprint(users)
+app.register_blueprint(previews)
 
 @app.route('/hello/', methods=['GET', 'POST'])
 def welcome():
