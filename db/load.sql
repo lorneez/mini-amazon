4553f1d26cf322037403253ecbba5eb2fbe10185
Jin Cho <jc695@duke.edu>
2021-11-11 21:50:44 -0500

    added dummy data for product reviews

--- db/load.sql
+++ db/load.sql
@@ -1,5 +1,6 @@
--- \COPY Users FROM '~/mini-amazon/db/data/Users.csv' WITH DELIMITER ',' NULL '' CSV
--- \COPY Products FROM '~/mini-amazon/db/data/Products.csv' WITH DELIMITER ',' NULL '' CSV
+\COPY Users FROM '~/mini-amazon/db/data/Users.csv' WITH DELIMITER ',' NULL '' CSV
+\COPY Products FROM '~/mini-amazon/db/data/Products.csv' WITH DELIMITER ',' NULL '' CSV
 \COPY OrderItem FROM '~/mini-amazon/db/data/OrderItem.csv' WITH DELIMITER ',' NULL '' CSV
--- \COPY SavedItem FROM '~/mini-amazon/db/data/SavedItem.csv' WITH DELIMITER ',' NULL '' CSV
--- \COPY CartItem FROM '~/mini-amazon/db/data/CartItem.csv' WITH DELIMITER ',' NULL '' CSV
+\COPY SavedItem FROM '~/mini-amazon/db/data/SavedItem.csv' WITH DELIMITER ',' NULL '' CSV
+\COPY CartItem FROM '~/mini-amazon/db/data/CartItem.csv' WITH DELIMITER ',' NULL '' CSV
+\COPY ProductReview FROM '~/mini-amazon/db/data/ProductReview.csv' WITH DELIMITER ',' NULL '' CSV
