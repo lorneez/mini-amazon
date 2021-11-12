61d907ba69db526b343795d8ea2e3e70c8d42d76
Jin Cho <jc695@duke.edu>
2021-11-11 21:54:15 -0500

    combine product reviews into product.py

--- app.py
+++ app.py
@@ -6,14 +6,12 @@ from backend.app.cart import cart
 from backend.app.orders import orders
 from backend.app.saved import saved
 from backend.app.users import users
-from backend.app.product_reviews import previews
 
 app.register_blueprint(products)
 app.register_blueprint(cart)
 app.register_blueprint(orders)
 app.register_blueprint(saved)
 app.register_blueprint(users)
-app.register_blueprint(previews)
 
 @app.route('/hello/', methods=['GET', 'POST'])
 def welcome():
