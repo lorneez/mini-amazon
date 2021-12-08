a02ed648e072061bb6fc3a0962074837a2060634
Sophia <swp17@duke.edu>
2021-12-07 19:32:56 -0500

    connected seller review

--- mini-amazon/src/components/Details.js
+++ mini-amazon/src/components/Details.js
@@ -3,24 +3,12 @@ import { RatingView } from 'react-simple-star-rating';
 import axios from "axios";
 import {AuthContext} from "../contexts/AuthContext";
 import ProductReviewModal from '../pages/product/ProductReviewModal';
+import CreateReview from '../pages/seller/CreateReview';
 
-function Details({id, title, price, rating, image}) {
+function Details({id, title, price, rating, image, seller}) {
     const auth = useContext(AuthContext);
     const { state } = auth;
     const { userId } = state;
-    // const [sellerdata, setsellerData] = useState([]);
-
-    // useEffect(async () => {
-    //     const result = await axios(
-    //         'http://localhost:5000/api/all_seller_reviews/?seller_id=' + userId, {
-    //             headers: {
-    //                 'Access-Control-Allow-Origin': '*',
-    //             },
-    //         }
-    //     );
-    //     setsellerData(result.data);
-    //     console.log(result.data)
-    // }, []);
     
     async function handleBuy() {
         const url = 'http://localhost:5000/api/buy_product/?product_id=' + id + '&user_id=' + userId + '&quantity=' + "1" 
@@ -75,6 +63,9 @@ console.log("Hello")
                     <button class="button is-warning is-medium is-rounded is-fullwidth" onClick={addToCart}>Add to Cart</button>
                     <div class="mt-2"></div>
                     <button class="button is-danger is-medium is-rounded is-fullwidth" onClick={() => handleBuy()}>Buy Now</button>
+                    </div>
+                    <div>
+                        {CreateReview(seller)}
                     </div>
                     <div>
                         {ProductReviewModal(id, userId)}
